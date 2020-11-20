1bbe5d8afede115da01f823ec053bb9390fcbce6
unknown <benthaier.ahmed@gmail.com>
2020-11-20 13:13:36 +0100

    Initial coomit

---
+++ src/Controller/PinsController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pin;
+use App\Form\PinType;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinsController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home", methods="GET")
+     */
+    public function index(PinRepository $pinRepo): Response
+    {
+        $pins = $pinRepo->findBy([], ['updatedAt' => 'DESC']);
+        return $this->render('pins/index.html.twig', compact('pins'));
+    }
+
+    /**
+     * @Route("/user/pins/{id<[0-9]+>}", name="app_pins_show", methods="GET")
+     */
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
+    }
+
+     /**
+     * @Route("/user/pins/create", name="app_pins_create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $pin = new Pin;
+
+        $form = $this->createForm(PinType::class, $pin);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            // $pin equivalent to $form->getData();
+            $pin->setUser($this->getUser());
+            $em->persist($pin);
+            $em->flush();
+
+            $this->addFlash('success', 'Pin successfully created');
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/user/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "PUT"})
+     */
+    public function edit(Pin $pin, Request $request, EntityManagerInterface $em): Response
+    {
+        if($pin->getUser() != $this->getUser()){
+            $this->addFlash('error', 'You can only edit you own pins.');
+            return $this->redirectToRoute("app_home");
+        }
+
+        $form = $this->createForm(PinType::class, $pin, [
+            'method' => 'PUT'
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+
+            $this->addFlash('success', 'Pin successfully updated');
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/edit.html.twig', [
+            'pin' => $pin,
+            'form' => $form->createView() ]);
+    }
+
+    /**
+     * @Route("/user/pins/{id<[0-9]+>}", name="app_pins_delete", methods="DELETE")
+     */
+    public function delete(Request $request,Pin $pin, EntityManagerInterface $em): Response
+    {
+        if($pin->getUser() != $this->getUser()){
+            $this->addFlash('error', 'You can only delete you own pins.');
+            return $this->redirectToRoute("app_home");
+        }
+
+        if($this->isCsrfTokenValid('delete_pin_' . $pin->getId(), $request->request->get('pin__token'))){
+            $em->remove($pin);
+            $em->flush();
+
+            $this->addFlash('info', 'Pin successfully deleted');
+        }
+
+        return $this->redirectToRoute("app_home");
+    }
+}
---
+++ src/Form/PinType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class PinType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            //->setAction()
+            //->setMethod
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG or PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'Delete',
+                'download_uri' => false,
+                'imagine_pattern' => 'squared_thumbnail_small'
+            ])
+            ->add('title')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Pin::class,
+        ]);
+    }
+}
